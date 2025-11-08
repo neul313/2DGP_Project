27bc276dacf6bffb577b95f483c3bdc7965efb74
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-08 15:56:39 +0900

    수업 코드 참조하여 속도에 영향을 받도록 idle 함수 작성

---
+++ 2DGP_Project/girl.py
@@ -0,0 +1,61 @@
+from pico2d import *
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+
+import game_world
+import game_framework
+
+# 달리기 시간
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# 액션 시간
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+class Idle:
+    def __init__(self, girl):
+        self.girl = girl
+
+    def enter(self, e):
+        pass
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        self.girl.frame = (self.girl.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
+
+    def draw(self):
+        if self.girl.face_dir == 1:  # right
+            self.girl.image.clip_draw(int(self.girl.frame) * (144/3), 192/4, (144/2), 192/3, self.girl.x, self.girl.y)
+        else:  # face_dir == -1: # left
+            self.girl.image.clip_draw(int(self.girl.frame) * (144/3), 192/4, (144/2), 192/3, self.girl.x, self.girl.y)
+
+
+class Girl:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def handle_event(self, event):
+        pass
+    def handle_collision(self, group, other):
+        pass
