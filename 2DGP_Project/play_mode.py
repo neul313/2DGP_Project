29f30eb5f3cc9cf77d618650df9b757d2c0fa98c
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-08 15:59:14 +0900

    수업 코드 참조하여 작성

---
+++ 2DGP_Project/play_mode.py
@@ -0,0 +1,42 @@
+import random
+from pico2d import *
+
+import game_framework
+import game_world
+
+from girl import Girl
+
+girl = None
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            girl.handle_event(event)
+
+def init():
+    global girl
+
+    girl = Girl()
+    game_world.add_object(girl, 1)
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
