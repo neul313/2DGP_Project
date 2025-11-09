f8bb62f6578cdf9ddc8e4acbae5cb39f21c61364
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-10 00:20:43 +0900

    보스 체력바 준비

--- 2DGP_Project/boss.py
+++ 2DGP_Project/boss.py
@@ -5,6 +5,7 @@ import game_framework
 import game_world
 from missile import Missile
 
+
 TIME_PER_ACTION = 1.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 2.0
@@ -18,6 +19,8 @@ class Boss:
         self.image = [load_image('boss1.png'),load_image('boss2.png')]
         self.frame =0.0
         self.x, self.y = 600, 300
+        self.max_hp = 100 #최대
+        self.hp = self.max_hp #현재 체력
 
         #미사일 타이머
         self.missile_timer = random.uniform(3.0, 5.0)
@@ -27,8 +30,11 @@ class Boss:
         self.missile_timer -= game_framework.frame_time
 
         if self.missile_timer <= 0:
-            self.missle_spawn()
+            self.missile_spawn()
             self.missile_timer = random.uniform(3.0, 5.0)
+
+        if self.hp <=0:
+            game_world.remove_object(self)
 
     def draw(self):
         image = self.image[int(self.frame)]
@@ -40,9 +46,15 @@ class Boss:
     def handle_collision(self, group, other):
         pass
 
-    def missle_spawn(self):
+    def missile_spawn(self):
         x = random.uniform(200,1000)
 
         missile = Missile(x,500)
         game_world.add_object(missile,1)
         game_world.add_collision_pair('missile:girl', missile, None)
+
+    def hp(self):
+        pass
+
+    def hp_min(self, n):
+        pass
