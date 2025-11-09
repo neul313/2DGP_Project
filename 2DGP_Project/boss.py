32a72c5861be8d0101fa9f066445763d12f72d11
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-09 21:28:23 +0900

    보스 뼈대 작성

---
+++ 2DGP_Project/boss.py
@@ -0,0 +1,28 @@
+from pico2d import *
+import random
+import math
+import game_framework
+import game_world
+
+TIME_PER_ACTION = 1.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 10.0
+
+class Boss:
+    def load_images(self):
+        pass
+
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_event(self):
+        pass
+
+    def handle_collision(self):
+        pass
