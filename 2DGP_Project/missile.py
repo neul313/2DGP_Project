4d4bafbd78a5d558204333c9efa2270b8a912237
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-09 22:53:59 +0900

    update작성, init작성

--- 2DGP_Project/missile.py
+++ 2DGP_Project/missile.py
@@ -1,3 +1,4 @@
+import pygame
 from pico2d import *
 import game_world
 import game_framework
@@ -5,11 +6,20 @@ import game_framework
 MISSILE_SPEED_PPS = 300
 
 class Missile:
+    image = None
+
     def __init__(self):
-        pass
+        if self.image is None:
+            self.image = load_image('attack.png')
+
+        self.x, self.y = x,y
+
 
     def update(self):
-        pass
+        self.y -= MISSILE_SPEED_PPS * game_framework.frame_time
+        # 임시로 90으로 설정
+        if self.y < 90:
+            game_world.remove_object(self)
 
     def draw(self):
         pass
@@ -17,5 +27,5 @@ class Missile:
     def get_bb(self):
         pass
 
-    def handle_collision(self):
+    def handle_collision(self, group, other):
         pass
