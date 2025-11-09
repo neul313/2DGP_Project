c33810bd13af2c86f7870cdfa511bba934cb0667
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-09 23:08:32 +0900

    미사일 3-5초 사이에 발사

--- 2DGP_Project/boss.py
+++ 2DGP_Project/boss.py
@@ -15,11 +15,19 @@ class Boss:
 
     def __init__(self):
         self.image = [load_image('boss1.png'),load_image('boss2.png')]
-
         self.frame =0
+        self.x, self.y = 600, 300
+
+        #미사일 타이머
+        self.missile_timer = random.randint(3, 5)
 
     def update(self):
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 2
+        self.missile_timer -= game_framework.frame_time
+
+        if self.missile_timer <= 0:
+            self.missle_spawn()
+            self.missile_timer = random.randint(3, 5)
 
     def draw(self):
         image = self.image[int(self.frame)]
@@ -29,4 +37,7 @@ class Boss:
         pass
 
     def handle_collision(self):
+        pass
+
+    def missle_spawn(self):
         pass
