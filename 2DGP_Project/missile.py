bb8838441b385e2a59e98149a287f3a6a5e28860
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-09 22:21:37 +0900

    미사일 클래스 생성 및 뼈대 작성

---
+++ 2DGP_Project/missile.py
@@ -0,0 +1,21 @@
+from pico2d import *
+import game_world
+import game_framework
+
+MISSILE_SPEED_PPS = 300
+
+class Missile:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass
+
+    def handle_collision(self):
+        pass
