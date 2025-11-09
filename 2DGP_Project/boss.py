27256d8ff97b1193949a54c8da9907e9d57b2381
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-09 22:02:15 +0900

    보스 애니메이션 그려내기

--- 2DGP_Project/boss.py
+++ 2DGP_Project/boss.py
@@ -4,7 +4,7 @@ import math
 import game_framework
 import game_world
 
-TIME_PER_ACTION = 1.5
+TIME_PER_ACTION = 5.0
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 10.0
 
@@ -14,14 +14,16 @@ class Boss:
         pass
 
     def __init__(self):
-        self.boss = Boss
-        self.image = load_image('boss1.png')
+        self.image = [load_image('boss1.png'),load_image('boss2.png')]
+
+        self.frame =0
 
     def update(self):
-        pass
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 2
 
     def draw(self):
-        self.image.draw(600, 300, 400,400)
+        image = self.image[int(self.frame)]
+        image.draw(600, 300, 400,400)
 
     def handle_event(self):
         pass
