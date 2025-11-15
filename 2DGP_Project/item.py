60901eeabd27c385b83182478598f63bc7673979
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-15 20:29:05 +0900

    아이템 코드 작성

--- 2DGP_Project/item.py
+++ 2DGP_Project/item.py
@@ -5,17 +5,29 @@ import game_framework
 class Item:
     image = None
 
-    def __init__(self, x, y):
-        pass
+    def __init__(self, x, y, cx, cy):
+        if Item.image is None:
+            Item.image = load_image('items.png')
+        self.x, self.y = x, y
+        self.clip_x = cx
+        self.clip_y = cy
+        self.size = 20
 
     def update(self):
         pass
 
     def draw(self):
-        pass
+        draw_x = self.x - game_framework.camera_x
+        draw_y = self.y - game_framework.camera_y
+
+        clip_bottom = self.image.h - self.clip_y - self.size
+
+        self.image.clip_draw(self.clip_x, clip_bottom, self.size, self.size,
+                            draw_x, draw_y, self.size, self.size)
 
     def get_bb(self):
-        pass
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
 
     def handle_collision(self, group, other):
-        pass
+        if group == 'girl:item':
+            game_world.remove_object(self)
