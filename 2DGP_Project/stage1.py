d9786c4dbd3367110a202e33a0b707f34780395c
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-15 15:08:32 +0900

    stage1준비

---
+++ 2DGP_Project/stage1.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+class Stage1:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+            pass
+
+    def handle_collision(self, group, other):
+        pass
