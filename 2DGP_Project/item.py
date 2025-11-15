fda7d363ca29032969eb960556062731aad4bc76
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-15 20:19:12 +0900

    아이템 추가 토대 작성

---
+++ 2DGP_Project/item.py
@@ -0,0 +1,21 @@
+from pico2d import *
+import game_world
+import game_framework
+
+class Item:
+    image = None
+
+    def __init__(self, x, y):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass
+
+    def handle_collision(self, group, other):
+        pass
