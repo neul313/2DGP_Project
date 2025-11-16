83f15be124a698daf27b6d3b9fb1d9b0847a6b42
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-16 15:40:31 +0900

    이미지 수정에 따른 코드 수정

--- 2DGP_Project/HP.py
+++ 2DGP_Project/HP.py
@@ -7,15 +7,21 @@ class Bar:
         self.bar = bar
 
         if Bar.images == None:
-            Bar.images = {}  # 딕셔너리로 만듦
+            Bar.images = {}
 
-            for i in range(7):
-                Bar.images[i] = load_image(f'hp_mp/{i + 1}.png')
+            Bar.images['hp'] = {}
+            Bar.images['mp'] = {}
+
+            for i in range(9):
+                Bar.images['hp'][i] = load_image(f'hp/{i}.png')
+
+            for i in range(9):
+                Bar.images['mp'][i] = load_image(f'mp/{i}.png')
 
         self.images = Bar.images
 
-        self.x = -50
-        self.y = 390
+        self.x = 130
+        self.y = 590
         self.width = 400
         self.height = 400
 
@@ -25,11 +31,40 @@ class Bar:
         elif self.bar == 'mp':
             self.max_mp=80
             self.current_mp = self.max_mp
+            self.y = 588
 
 
     def draw(self):
-       pass
+        index = 0
+        image_set = None
 
+        if self.bar == 'hp':
+            index = (self.current_hp/10)
+            image_set = self.images['hp']
+
+        elif self.bar == 'mp':
+            index = (self.current_mp/10)
+            image_set = self.images['mp']
+
+        index = clamp(0, index, 8)
+
+        if image_set:
+            image_set[index].draw(self.x, self.y, self.width, self.height)
+
+    def update(self):
+        if self.bar == 'hp':
+            if play_mode.girl:
+                if hasattr(play_mode.girl, 'hp'):
+                    self.current_hp = play_mode.girl.hp
+            self.current_hp = clamp(0, self.current_hp, self.max_hp)
+
+        elif self.bar == 'mp':
+            if play_mode.girl:
+                if hasattr(play_mode.girl, 'mp'):
+                    self.current_mp = play_mode.girl.mp
+                else:
+                    self.current_mp = 80  # mp가 없으면 테스트용으로 80
+            self.current_mp = clamp(0, self.current_mp, self.max_mp)
 
     def update(self):
         if self.bar == 'hp':
--- 2DGP_Project/Project.py
+++ 2DGP_Project/Project.py
@@ -6,3 +6,9 @@ import play_mode as start_mode
 open_canvas(1200, 700)
 game_framework.run(start_mode)
 close_canvas()
+
+
+
+
+
+
