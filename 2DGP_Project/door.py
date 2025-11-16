c993a13bc506170cd955d6c1637e4e2b1211a135
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-16 22:27:18 +0900

    stage1막힌곳 뼈대 제작

---
+++ 2DGP_Project/door.py
@@ -0,0 +1,22 @@
+from pico2d import *
+import game_framework
+import game_world
+
+class Door:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass
+
+    def handle_collision(self):
+        pass
+
+    def unlock(self):
+        pass
