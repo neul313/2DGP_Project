46f04bd07b771c21919f96084908c0f0f223c5ad
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-16 14:08:36 +0900

    hp이미지

--- 2DGP_Project/HP.py
+++ 2DGP_Project/HP.py
@@ -4,7 +4,7 @@ import play_mode
 class Bar:
     def __init__(self):
         self.image = load_image('hp.png')
-        self.x = 10
+        self.x = -50
         self.y = 390
         self.width = 400
         self.height = 400
@@ -14,17 +14,12 @@ class Bar:
         self.current_hp = self.max_hp
         self.current_mp = self.max_mp
 
-        # hp 표시 직사각형 두께
-        self.offset_x = 5  # 왼쪽 테두리 두께
-        self.offset_y = 5  # 아래쪽 테두리 두께
-
     def draw(self):
         self.image.clip_draw_to_origin(0, 0, self.image.w, self.image.h,self.x, self.y,self.width, self.height)
 
-
     def update(self):
         if play_mode.girl:
-            # girl.py에 self.hp가 정의되어 있다고 가정
+            # girl.py에 self.hp 정의함
             if hasattr(play_mode.girl, 'hp'):
                 self.current_hp = play_mode.girl.hp
         self.current_hp = clamp(0, self.current_hp, self.max_hp)
