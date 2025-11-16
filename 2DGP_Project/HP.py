082e2b38f1c3e190b5b286aa8a209610290d5e0f
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-16 12:42:17 +0900

    hp,mp 준비

---
+++ 2DGP_Project/HP.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+class Bar:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+    def handle_event(self, event):
+        pass
