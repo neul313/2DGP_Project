25198f5a196c5c376c9d9ac50f4ed0c8f4d242c0
Jo shinBe <tls3123@tukorea.ac.kr>
2025-12-01 00:39:45 +0900

    cannot import name 'Bar' from 'HP' 때문에 수정

--- 2DGP_Project/HP.py
+++ 2DGP_Project/HP.py
@@ -1,15 +1,15 @@
 from pico2d import *
-import play_mode
-import game_framework
+import game_framework  # play_mode 대신 game_framework만 import
+
 
 class Bar:
     images = None
+
     def __init__(self, bar):
         self.bar = bar
 
         if Bar.images == None:
             Bar.images = {}
-
             Bar.images['hp'] = {}
             Bar.images['mp'] = {}
 
@@ -27,24 +27,27 @@ class Bar:
         self.height = 400
 
         if self.bar == 'hp':
-            self.max_hp=80
+            self.max_hp = 80
             self.current_hp = self.max_hp
         elif self.bar == 'mp':
-            self.max_mp=80
+            self.max_mp = 80
             self.current_mp = self.max_mp
             self.y = 588
-
 
     def draw(self):
         index = 0
         image_set = None
 
         if self.bar == 'hp':
-            index = (self.current_hp/10)
+            if self.max_hp > 0:
+                ratio = self.current_hp / self.max_hp
+                index = int(ratio * 8)
             image_set = self.images['hp']
 
         elif self.bar == 'mp':
-            index = (self.current_mp/10)
+            if self.max_mp > 0:
+                ratio = self.current_mp / self.max_mp
+                index = int(ratio * 8)
             image_set = self.images['mp']
 
         index = clamp(0, index, 8)
@@ -53,23 +56,18 @@ class Bar:
             image_set[index].draw(self.x, self.y, self.width, self.height)
 
     def update(self):
-
         if 'girl' in game_framework.share:
             girl = game_framework.share['girl']
 
-        if self.bar == 'hp':
-            if play_mode.girl:
-                if hasattr(play_mode.girl, 'hp'):
-                    self.current_hp = play_mode.girl.hp
-            self.current_hp = clamp(0, self.current_hp, self.max_hp)
+            if self.bar == 'hp':
+                if hasattr(girl, 'hp'):
+                    self.current_hp = girl.hp
+                self.current_hp = clamp(0, self.current_hp, self.max_hp)
 
-        elif self.bar == 'mp':
-            if play_mode.girl:
-                if hasattr(play_mode.girl, 'mp'):
-                    self.current_mp = play_mode.girl.mp
-                else:
-                    self.current_mp = 80  # mp가 없으면 테스트용으로 80
-            self.current_mp = clamp(0, self.current_mp, self.max_mp)
+            elif self.bar == 'mp':
+                if hasattr(girl, 'mp'):
+                    self.current_mp = girl.mp
+                self.current_mp = clamp(0, self.current_mp, self.max_mp)
 
     def handle_event(self, event):
         pass
