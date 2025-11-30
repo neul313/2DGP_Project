ac418651ed5d601ad3b0b5f1a560276ce37ce5d8
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-30 23:01:39 +0900

    stage2_mode복사후 맞게 수정함// 추가적인 수정 필요 뼈대만 만든셈

---
+++ 2DGP_Project/stage3_mode.py
@@ -0,0 +1,98 @@
+import random
+from pico2d import *
+
+import game_framework
+import game_world
+from girl import Girl
+from stage2 import Stage2
+from item import Item
+from inventory import Inventory
+from door import Door
+from HP import Bar
+from boss2 import Boss
+import stage3
+
+girl = None
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            if 'girl' in game_framework.share:
+                game_framework.share['girl'].handle_event(event)
+
+
+def init():
+    import play_mode
+
+    global girl
+    game_world.clear()
+
+    if 'girl' not in game_framework.share:
+        girl = Girl()
+        game_framework.share['girl'] = girl
+        inv = Inventory(girl)
+        game_framework.share['inventory_ui'] = inv
+
+        hp = Bar('hp')
+        mp = Bar('mp')
+        game_framework.share['hp'] = hp
+        game_framework.share['mp'] = mp
+    girl = game_framework.share['girl']
+
+    girl.x, girl.y = 50, 100
+    girl.face_dir = 1
+    game_world.add_object(girl, 1)
+
+    stage2_background = Stage2()
+    game_world.add_object(stage2_background, 0)
+    game_framework.share['background'] = stage2_background
+
+    stage2_background.set_center_object(girl)
+
+    stage2_background = Stage2()
+    game_world.add_object(stage2_background, 0)
+    game_framework.share['background'] = stage2_background
+    stage2_background.set_center_object(girl)
+
+    game_world.add_object(game_framework.share['inventory_ui'], 3)
+    game_world.add_object(game_framework.share['hp'], 3)
+    game_world.add_object(game_framework.share['mp'], 3)
+
+    game_world.add_collision_pair('girl:item', girl, None)
+    game_world.add_collision_pair('missile:girl', None, girl)
+    game_world.add_collision_pair('girl:door', girl, None)
+
+    item_gun = Item(555, 270, 0, 0, 'gun', 10)
+    game_world.add_object(item_gun, 1)
+    game_world.add_collision_pair('girl:item', None, item_gun)
+
+    boss = Boss(300,280)
+    game_world.add_object(boss, 1)
+    game_world.add_collision_pair('tang:boss', None, boss)
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def finish():
+    game_world.clear()
+
+def pause():
+    pass
+
+def resume():
+    pass
